89b7a1fbf0e1915e58e54e80d7f8f750f5aa7494
SuperCodeComput <112727452+SuperCodeComputer@users.noreply.github.com>
2022-09-09 19:22:05 +0000

    update

---
+++ prog52a.py
@@ -0,0 +1,13 @@
+length = int(input("please enter length: "))
+width = int(input("please enter width: "))
+
+area = length * width
+perim = 2 * length + 2 * width
+
+print("The length is", length)
+print("the width is", area)
+
+print("\nthe area is", area)
+print("the perimeter is", perim)
+
+# math ops: + - * %  ** (pow) // (floor divide)
